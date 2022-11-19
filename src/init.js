e852f2059a36a0c504f2cd24dfab38c00b7f6379
BradySBaker <bradysamuelbaker@gmail.com>
2022-11-19 12:45:27 -0800

    "Complete publishing dance party",  "Complete Bare Minimum Requirements"

--- src/init.js
+++ src/init.js
@@ -26,19 +26,23 @@ $(document).ready(function() {
 
 
   //Interact Button Click
-  $('.interactButton').on('click', function(event) {
+  $('#interactButton').on('click', function(event) {
     dancers.forEach(function(curDancer) {
       if (curDancer.dancerMakerFunctionName === 'makeInteractDancer') {
         curDancer.interact();
+        curDancer.timeBetweenSteps = 100;
       }
     });
   });
 
   //Lineup Button Click
-  $('.lineupButton').on('click', function(event) {
+  $('#lineupButton').on('click', function(event) {
     var topPos = 10;
     var leftPos = 10;
     dancers.forEach(function(curDancer) {
+      if (curDancer.dancerMakerFunctionName === 'makeInteractDancer') {
+        curDancer.interacted = false;
+      }
       var curSize = curDancer.$node.css('border-width');
       curSize = curSize.slice(0, curSize.length - 2);
       curSize = Number(curSize);
@@ -56,21 +60,56 @@ $(document).ready(function() {
   //Mouser Dancer
   $('body').on('mouseover', '.mouseDancer', function(event) {
     var settings = {
-      'border-color': 'orange',
-      'border-radius': '5px',
-      'border-width': '100px'
+      'background-image': 'url("mario.gif")',
+      'min-width': '100px',
+      'min-height': '100px',
+      'background-size': '100px',
+      'background-position': 'center'
     };
     $(event.target).css(settings);
   });
 
   $('body').on('mouseout', '.mouseDancer', function(event) {
     var settings = {
-      'border-color': 'red',
-      'border-radius': '10px',
-      'border-width': '10px'
+      'background-image': '',
+      'background-size': '',
+      'min-width': '',
+      'min-height': ''
     };
     $(event.target).css(settings);
   });
 
-});
+  //Interact Speed Button Click
+  $('#interactSpeed').on('click', function(event) {
 
+    dancers.forEach(function(curDancer) {
+      if (curDancer.dancerMakerFunctionName === 'makeInteractDancer') {
+        var speedArr = [100, 50, 30, 10, 5];
+        curDancer.timeBetweenSteps = speedArr[curDancer.speedIterator];
+        if (curDancer.speedIterator === 4) {
+          curDancer.speedIterator = 0;
+        } else {
+          curDancer.speedIterator++;
+        }
+      }
+    });
+  });
+
+  //Interact Size Button Click
+  $('#interactSize').on('click', function(event) {
+
+    dancers.forEach(function(curDancer) {
+      if (curDancer.dancerMakerFunctionName === 'makeInteractDancer') {
+        var sizeArr = [30, 50, 100, 130, 200];
+        curDancer.size = sizeArr[curDancer.sizeIterator];
+        curDancer.rotationPattern = curDancer.createRotationPattern();
+        if (curDancer.sizeIterator === 4) {
+          curDancer.sizeIterator = 0;
+        } else {
+          curDancer.sizeIterator++;
+        }
+      }
+    });
+  });
+
+});
--- src/interactDancer.js
+++ src/interactDancer.js
@@ -2,9 +2,13 @@ var makeInteractDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
 };
 
+
 makeInteractDancer.prototype = Object.create(makeDancer.prototype);
 makeInteractDancer.prototype.constructor = makeInteractDancer;
-makeInteractDancer.prototype.togglePos = false;
+makeInteractDancer.prototype.iterator = 1;
+makeInteractDancer.prototype.size = 30;
+makeInteractDancer.prototype.speedIterator = 1;
+makeInteractDancer.prototype.sizeIterator = 1;
 makeInteractDancer.prototype.interacted = false;
 //At current dancer = interact
 
@@ -40,7 +44,11 @@ makeInteractDancer.prototype.interact = function() {
       }
     }
   });
-  this.setPosition(curMin.top, curMin.left + 30);
+  this.setPosition(curMin.top + 30, curMin.left);
+  this.originalTop = this.top - 32;
+  this.originalLeft = this.left;
+  //Call rotation creator function
+  this.rotationPattern = this.createRotationPattern();
   this.interacted = true;
 };
 
@@ -48,15 +56,78 @@ makeInteractDancer.prototype.interact = function() {
 makeInteractDancer.prototype.step = function() {
   makeDancer.prototype.step.call(this);
   if (this.interacted === true) {
-    if (this.togglePos === false) {
-      this.setPosition(this.top + 30, this.left);
-      this.togglePos = true;
+    var topArr = this.rotationPattern.top;
+    var leftArr = this.rotationPattern.left;
+    if (this.iterator < topArr.length) {
+      this.top = this.originalTop + topArr[this.iterator];
+      this.left = this.originalLeft + leftArr[this.iterator];
+      this.iterator++;
     } else {
-      this.setPosition(this.top - 30, this.left);
-      this.togglePos = false;
+      this.iterator = 1;
     }
   }
+    this.setPosition(this.top, this.left);
 };
 //
 
+makeInteractDancer.prototype.createRotationPattern = function() {
+  //Starts Above
+  var posObj = {top: [this.size], left: [0]}
+  var i = 1;
+  while (i - 1 !== this.size * 4) {
+      var prevTop = posObj.top[i - 1];
+      var prevLeft = posObj.left[i - 1];
+      //To the Right
+      if (i < this.size) {
+        posObj.top.push(prevTop - 1);
+        posObj.left.push(prevLeft + 1);
+        //To the Bottom
+      } else if (i < this.size * 2) {
+        posObj.top.push(prevTop - 1);
+        posObj.left.push(prevLeft - 1);
+        //To the Left
+      } else if (i < this.size * 3) {
+        posObj.top.push(prevTop + 1);
+        posObj.left.push(prevLeft - 1);
+        //Back to Top
+      } else {
+        posObj.top.push(prevTop + 1);
+        posObj.left.push(prevLeft + 1);
+      }
+      i++;
+  }
+  return posObj;
+};
 
+//Old pattern
+  /*if (this.interacted === true) {
+    //First move
+    if (this.iterator === 0) {
+      this.top -= 30;
+      this.left += 30;
+      this.iterator++;
+    } //Second move
+    else if (this.iterator === 1) {
+      this.top -= 30;
+      this.left -= 30;
+      this.iterator++;
+    } //Third Move
+    else if (this.iterator === 2) {
+      this.top += 30;
+      this.left -= 30;
+      this.iterator++;
+    } //Reset Position
+    else if (this.iterator === 3) {
+      this.top += 30;
+      this.left += 30;
+      this.iterator = 0;
+    }*/
+
+//Top starts at +30
+//Left starts at 0
+
+//Subtract by 1 until top equals 0
+//Add by 1 until left equals 30
+
+//Push last iteration minus 1 for top
+//Push last iteration plus 1 for left
